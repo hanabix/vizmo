e9a75b0c44ac73df19d972b9fbef2ac81a0a5965
zhongl <zhong.lunfu@gmail.com>
2025-02-18 15:30:39 +0800

    Add navigation bar.
    > App.vue 界面顶部添加一个导航栏
    
    > App.vue 导航栏背景色该用 Dark 风格，主题文字靠左，右侧添加一个菜单图标按钮
    
    > App.vue 6-16 保证效果不变的前提下，精简代码
    
    > App.vue 在保证效果不变的前提下，删除div，令代码结构更简单

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@ import PWABadge from './components/PWABadge.vue'
 </script>
 
 <template>
+  <nav class="fixed inset-x-0 top-0 z-50 bg-gray-600 p-4 shadow flex justify-between items-center">
+    <h1 class="text-xl font-bold text-white">WitMotion BLE</h1>
+    <button class="p-2 rounded-lg hover:bg-gray-800">
+      <svg class="h-6 w-6 text-white" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+        <path d="M4 6h16M4 12h16M4 18h16" />
+      </svg>
+    </button>
+  </nav>
   <PWABadge />
 </template>
 
