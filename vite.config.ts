080808e69585579c7c2eaf2a52923eac808033e6
zhongl <zhong.lunfu@gmail.com>
2025-02-18 14:47:06 +0800

    Install tailwindcss.
    screenshot: https://tailwindcss.com/docs/installation/using-vite
    
    > 参考图片中的描述，在本项目中安装 tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,44 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  server: {
+    https: true,
+    host: "0.0.0.0"
+  },
+  plugins: [
+    tailwindcss(),
+    vue(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'WitMotion BLE',
-      short_name: 'wble',
-      description: 'Manage low energy bluetooth devices of WitMotion',
-      theme_color: '#0000FF',
-    },
+      manifest: {
+        name: 'WitMotion BLE',
+        short_name: 'wble',
+        description: 'Manage low energy bluetooth devices of WitMotion',
+        theme_color: '#0000FF',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    })
+  ]
 })
